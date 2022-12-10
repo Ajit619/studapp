030b5cd4e18830e61e029926f77ee0bf7835f636
Ajitkumar Sharma <77828414+Ajit619@users.noreply.github.com>
2022-12-10 22:27:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 var expressLayouts = require('express-ejs-layouts');
 
